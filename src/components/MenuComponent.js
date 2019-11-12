0123c2585e32e0588eade574319aa0e6deb0dc51
saqib.sidhu81 <saqib.sidhu81@gmail.com>
2019-11-12 15:54:51 +0500

    Redux Thunk

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -4,6 +4,7 @@ import {
     CardTitle, Breadcrumb, BreadcrumbItem
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
 function RenderMenuItem({ dish }) {
     return (
@@ -20,12 +21,33 @@ function RenderMenuItem({ dish }) {
 
 const Menu = (props) => {
 
-    const menu = props.dishes.map((dish) => {
-        return (
-            <div className="col-12 col-md-5 m-1" key={dish.id}>
-                <RenderMenuItem dish={dish} onClick={props.onClick} />
-            </div>
-        );
+    const menu = props.dishes.dishes.map((dish) => {
+        if (props.dishes.isLoading) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        else if (props.dishes.errMess) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12">
+                            <h4>{props.dishes.errMess}</h4>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+        else
+            return (
+                <div className="col-12 col-md-5 m-1" key={dish.id}>
+                    <RenderMenuItem dish={dish} onClick={props.onClick} />
+                </div>
+            );
     });
 
     return (
@@ -38,7 +60,7 @@ const Menu = (props) => {
                 <div className="col-12">
                     <h3>Menu</h3>
                     <hr />
-                </div>                
+                </div>
             </div>
             <div className="row">
                 {menu}
